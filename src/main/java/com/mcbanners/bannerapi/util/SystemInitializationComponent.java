8980173937a8b2d6d6b5710220c828bbdb6e42fb
simpleauthority <jacob@algorithmjunkie.com>
2020-07-08 20:44:22 -0700

    Copy fonts on boot and use those instead of copying temporary files every time they are needed

---
+++ src/main/java/com/mcbanners/bannerapi/util/SystemInitializationComponent.java
@@ -0,0 +1,41 @@
+package com.mcbanners.bannerapi.util;
+
+import com.mcbanners.bannerapi.banner.BannerFontFace;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+@Component
+public class SystemInitializationComponent {
+    @PostConstruct
+    public void run() {
+        copyFontFiles();
+    }
+
+    private void copyFontFiles() {
+        File directory = new File("fonts");
+        if (!directory.exists() && !directory.mkdir()) {
+            throw new RuntimeException("Failed to create fonts directory on disk.");
+        }
+
+        for (BannerFontFace face : BannerFontFace.values()) {
+            try {
+                File regular = face.getFile(false);
+                if (!regular.exists()) {
+                    Files.copy(getClass().getResourceAsStream("/fonts/" + face.getFileName(false)), regular.toPath());
+                }
+
+                File bold = face.getFile(true);
+                if (!bold.exists()) {
+                    Files.copy(getClass().getResourceAsStream("/fonts/" + face.getFileName(true)), bold.toPath());
+                }
+            } catch (IOException e) {
+                System.err.println("Failed to copy file(s) for font " + face.name());
+                e.printStackTrace();
+            }
+        }
+    }
+}
