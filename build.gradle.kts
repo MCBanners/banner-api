353129d67d8dd2dbc28fd647f5d513c0262e7d4d
Glare <admin@glaremasters.me>
2024-06-09 18:54:12 -0500

    chore(deps): update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-	id("org.springframework.boot") version "3.2.1"
-	id("io.spring.dependency-management") version "1.1.4"
-	id("com.github.ben-manes.versions") version "0.50.0"
+	id("org.springframework.boot") version "3.3.0"
+	id("io.spring.dependency-management") version "1.1.5"
+	id("com.github.ben-manes.versions") version "0.51.0"
 	id("java")
 }
 
@@ -20,7 +20,7 @@ repositories {
 }
 
 dependencies {
-	implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2023.0.0"))
+	implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2023.0.2"))
 
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-cache")
@@ -28,18 +28,18 @@ dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 	implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
-	implementation("org.mariadb.jdbc:mariadb-java-client:3.3.2")
-	implementation("org.flywaydb:flyway-mysql:10.4.1")
-	implementation("org.flywaydb:flyway-core:10.4.1")
-	implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+	implementation("org.mariadb.jdbc:mariadb-java-client:3.4.0")
+	implementation("org.flywaydb:flyway-mysql:10.14.0")
+	implementation("org.flywaydb:flyway-core:10.14.0")
+	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
 	implementation("com.github.xMrAfonso:Hangar4J:1.2.3") {
 		exclude(group = "com.google.code.gson", module = "gson")
 	}
-	implementation("com.google.code.gson:gson:2.10.1")
-	implementation("com.github.usefulness:webp-imageio:0.6.0")
+	implementation("com.google.code.gson:gson:2.11.0")
+	implementation("com.github.usefulness:webp-imageio:0.8.0")
 
-	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
-	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
