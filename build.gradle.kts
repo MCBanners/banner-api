dd5b25adee69ac4b1be04311a185e7a276a2b945
Glare <admin@glaremasters.me>
2024-06-23 18:46:15 -0500

    chore(deps): update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	id("org.springframework.boot") version "3.3.0"
+	id("org.springframework.boot") version "3.3.1"
 	id("io.spring.dependency-management") version "1.1.5"
 	id("com.github.ben-manes.versions") version "0.51.0"
 	id("java")
@@ -29,17 +29,17 @@ dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 	implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
 	implementation("org.mariadb.jdbc:mariadb-java-client:3.4.0")
-	implementation("org.flywaydb:flyway-mysql:10.14.0")
-	implementation("org.flywaydb:flyway-core:10.14.0")
-	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+	implementation("org.flywaydb:flyway-mysql:10.15.0")
+	implementation("org.flywaydb:flyway-core:10.15.0")
+	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
 	implementation("com.github.xMrAfonso:Hangar4J:1.2.3") {
 		exclude(group = "com.google.code.gson", module = "gson")
 	}
 	implementation("com.google.code.gson:gson:2.11.0")
 	implementation("com.github.usefulness:webp-imageio:0.8.0")
 
-	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
-	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
