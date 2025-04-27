c634240c14601e8b89bae59e1642adafe10a5b9d
Glare <admin@glaremasters.me>
2025-04-27 15:10:54 -0500

    chore(deps): update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-	id("org.springframework.boot") version "3.3.2"
-	id("io.spring.dependency-management") version "1.1.6"
-	id("com.github.ben-manes.versions") version "0.51.0"
+	id("org.springframework.boot") version "3.4.5"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("com.github.ben-manes.versions") version "0.52.0"
 	id("java")
 }
 
@@ -20,23 +20,23 @@ repositories {
 }
 
 dependencies {
-	implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2023.0.3"))
+	implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2024.0.1"))
 
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-cache")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
-	implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
-	implementation("org.mariadb.jdbc:mariadb-java-client:3.4.1")
-	implementation("org.flywaydb:flyway-mysql:10.16.0")
-	implementation("org.flywaydb:flyway-core:10.16.0")
+	implementation("com.github.ben-manes.caffeine:caffeine:3.2.0")
+	implementation("org.mariadb.jdbc:mariadb-java-client:3.5.3")
+	implementation("org.flywaydb:flyway-mysql:11.8.0")
+	implementation("org.flywaydb:flyway-core:11.8.0")
 	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
 	implementation("com.github.xMrAfonso:Hangar4J:1.2.3") {
 		exclude(group = "com.google.code.gson", module = "gson")
 	}
-	implementation("com.google.code.gson:gson:2.11.0")
-	implementation("com.github.usefulness:webp-imageio:0.8.0")
+	implementation("com.google.code.gson:gson:2.13.1")
+	implementation("com.github.usefulness:webp-imageio:0.9.0")
 
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
